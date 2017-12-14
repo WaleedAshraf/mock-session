2bce982beaa95ae6d98e4add5a17a1cda380df98
Waleed Ashraf <waleedashraf@outlook.com>
2017-12-15 02:09:09 +0500

    mock-session initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+/*!
+ * mock-session
+ * Copyright(c) 2017 Waleed Ashraf
+ * MIT Licensed
+ */
+
+let Keygrip = require('keygrip');
+let Buffer = require('safe-buffer');
+
+function MakeCookies(name, keys, data) {
+  let cookie = Buffer.from(JSON.stringify(data)).toString('base64'); // base64 converted value of cookie  
+  let kg = Keygrip([keys]); // same key as your are using in app
+  let hash = kg.sign(cookie);
+
+  return name + '=' + cookie + '; ' + name + '.sig=' + hash + ';';
+}
+
+module.exports = MakeCookies
