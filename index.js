1392d3fb5dbc52dbb6dc219b87d62f46fab27015
Waleed Ashraf <waleedashraf@outlook.com>
2017-12-15 03:29:32 +0500

    fixes added

--- index.js
+++ index.js
@@ -5,13 +5,12 @@
  */
 
 let Keygrip = require('keygrip');
-let Buffer = require('safe-buffer');
+let Buffer = require('safe-buffer').Buffer;
 
 function MakeCookies(name, keys, data) {
   let cookie = Buffer.from(JSON.stringify(data)).toString('base64'); // base64 converted value of cookie  
   let kg = Keygrip([keys]); // same key as your are using in app
-  let hash = kg.sign(cookie);
-
+  let hash = kg.sign(name + '=' + cookie);
   return name + '=' + cookie + '; ' + name + '.sig=' + hash + ';';
 }
 
